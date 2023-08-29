207c1793be6ab4ba581effc0cbab191d45222e8e
jsj9871 <75598682+jsj9871@users.noreply.github.com>
2023-08-29 10:33:43 +0900

    머지 완

--- SunHaven/SunHaven/Resources.cpp
+++ SunHaven/SunHaven/Resources.cpp
@@ -3,31 +3,6 @@
 
 HRESULT Resources::init(void)
 {
-<<<<<<< HEAD
-	// 타이틀
-	IMAGEMANAGER->addImage("TITLE", "Resources/Images/Title/Title.bmp",
-		873 * 2, 360 * 2);	// 배경
-
-	IMAGEMANAGER->addGPFrameImage("TITLE_LOGO", "Resources/Images/Title/TitleLogo.png",
-		0, 0,
-		1, 1,
-		false, NULL, RotateNoneFlipNone);	// 타이틀 로고
-
-	IMAGEMANAGER->addImage("TITLE_MENU", "Resources/Images/Title/Title_Menu.bmp",
-		145 * 2, 40,
-		true, RGB(255, 0, 255));	// 메뉴
-
-//<<<<<<< HEAD
-	IMAGEMANAGER->addGPFrameImage("TITLE_BUTTON", "Resources/Images/Title/Title_Button.png",
-		0, 0,
-		1, 1,
-		false, NULL, RotateNoneFlipNone);	// 메뉴 위에 마우스 포인터 위치 시
-
-//=======
-	IMAGEMANAGER->addImage("TITLE_MENU", "Resources/Images/Title/Title_Menu.bmp",
-		145 * 1, 40 * 1,
-		true, RGB(255, 0, 255));	// 메뉴 클릭
-=======
 	// ====================TitleScene====================
 	// Image
 	IMAGEMANAGER->addGPFrameImage("TitleLogo", "Resources/Images/UI/TitleLogo.png",
@@ -43,7 +18,6 @@ HRESULT Resources::init(void)
 		0, 0,
 		1, 1,
 		false, NULL, RotateNoneFlipNone);	// 메뉴 위에 마우스 포인터 위치 시
->>>>>>> main
 	IMAGEMANAGER->addImage("Cursor", "Resources/Images/UI/Cursor.bmp",
 		32, 32,
 		true, RGB(255, 0, 255));	// 커서
@@ -56,9 +30,6 @@ HRESULT Resources::init(void)
 		true, RGB(255, 0, 255));
 	// Sound
 
-<<<<<<< HEAD
-//>>>>>>> PSH
-=======
 	// ====================DynusScene====================
 	// Image
 
@@ -73,7 +44,6 @@ HRESULT Resources::init(void)
 	// Image
 
 	// Sound
->>>>>>> main
 
 	return S_OK;
 }
